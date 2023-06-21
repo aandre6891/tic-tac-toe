297d0df67eb2cb1fd075dbb693bbaa334e9f9795
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-21 10:38:57 +0100

    added tests and Player class

---
+++ src/player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor() {
+    this.player1 = "";
+    this.player2 = "";
+  }
+
+  setPlayer1(player1) {
+    this.player1 = player1;
+  }
+
+  setPlayer2(player2) {
+    this.player2 = player2;
+  }
+}
+
+module.exports = Player;
---
+++ tests/player.test.js
@@ -0,0 +1,19 @@
+const Player = require("../src/player");
+
+describe("Player", () => {
+  it("initially the players are not assigned", () => {
+    const player = new Player();
+    expect(player.player1).toBe("");
+    expect(player.player2).toBe("");
+  });
+
+  it("players are correctly assigned", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+    player1.setPlayer1("Andrea");
+    player2.setPlayer2("Chiara");
+
+    expect(player1.player1).toBe("Andrea");
+    expect(player2.player2).toBe("Chiara");
+  });
+});
