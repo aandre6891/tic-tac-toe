81e25bdb9f6d45e47861e70ab29d819a7a7be306
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-23 09:25:32 +0100

    added more test for different moves and completed playRound to stop the round if one player wins

--- src/round.js
+++ src/round.js
@@ -1,12 +1,11 @@
 const Move = require("./move");
 const readline = require("readline");
-const winningSequences = require('./winningSequences.js');
+const winningSequences = require("./winningSequences.js");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 
 class Round {
   constructor() {
@@ -20,48 +19,60 @@ class Round {
       rl.question("Player 1, make your move: ", (playerInput) => {
         console.log("Input dell'utente:", playerInput);
         this.move.addMove("Player1", playerInput);
-        if (this.move.movesCounter === 9) {
+        if (this.move.movesCounter === 9 || this.player1WinChecker() === true) {
+          this.round++;
           this.displayResult();
+          rl.close();
         } else {
           rl.question("Player 2, make your move: ", (playerInput) => {
             console.log("Input dell'utente:", playerInput);
             this.move.addMove("Player2", playerInput);
-            this.playRound();
+            if (this.player2WinChecker() === true) {
+              this.round++;
+              this.displayResult();
+              rl.close();
+            } else {
+              this.playRound();
+            }
           });
         }
       });
-    } else {
-      this.displayResult();
     }
   }
 
   player1WinChecker() {
-    const player1Moves = this.move.movesList.filter((singleMove) => singleMove.player === "Player1").map((player1Move) => player1Move.move);
+    const player1Moves = this.move.movesList
+      .filter((singleMove) => singleMove.player === "Player1")
+      .map((player1Move) => player1Move.move);
     for (const sequence of this.winningSequences) {
-      const isWinningSequence = sequence.every((number) => player1Moves.includes(number));
-      if (isWinningSequence) {
-        return true;
-      } 
-    }
-    return false;
-  }
-  
-  player2WinChecker() {
-    const player2Moves = this.move.movesList.filter((singleMove) => singleMove.player === "Player2").map((player2Move) => player2Move.move);
-    for (const sequence of this.winningSequences) {
-      const isWinningSequence = sequence.every((number) => player2Moves.includes(number));
+      const isWinningSequence = sequence.every((number) =>
+        player1Moves.includes(number)
+      );
       if (isWinningSequence) {
         return true;
       }
     }
-    return false; 
+    return false;
   }
-  
+
+  player2WinChecker() {
+    const player2Moves = this.move.movesList
+      .filter((singleMove) => singleMove.player === "Player2")
+      .map((player2Move) => player2Move.move);
+    for (const sequence of this.winningSequences) {
+      const isWinningSequence = sequence.every((number) =>
+        player2Moves.includes(number)
+      );
+      if (isWinningSequence) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   displayResult() {
-    rl.close();
-    this.round++ 
-    console.log(this.player1WinChecker())
-    console.log(this.player2WinChecker())
+    console.log(this.player1WinChecker());
+    console.log(this.player2WinChecker());
     console.log("Round:", this.round);
   }
 }
--- tests/round.test.js
+++ tests/round.test.js
@@ -30,7 +30,7 @@ describe("Round", () => {
   
   });
 
-  test("player1WinChecker should return true and player2WinChecker should return true", () => {
+  test("both players should win", () => {
     const mockMove = jest.fn().mockImplementation(() => ({
       movesList: [
         { player: "Player1", move: "4" }, // winning combination Player1
@@ -42,6 +42,29 @@ describe("Round", () => {
         { player: "Player1", move: "7" }, // winning combination Player1
         { player: "Player2", move: "6" },  // winning combination Player2
         { player: "Player1", move: "8" },
+      ],
+    }));
+
+    const round = new Round();
+    round.move = mockMove();
+    const response1 = round.player1WinChecker();
+    expect(response1).toBe(true);
+    const response2 = round.player2WinChecker();
+    expect(response2).toBe(true);
+  });
+  
+  test("only player2WinChecker should return true", () => {
+    const mockMove = jest.fn().mockImplementation(() => ({
+      movesList: [
+        { player: "Player1", move: "1" },
+        { player: "Player2", move: "8" },  // winning combination Player2
+        { player: "Player1", move: "6" },
+        { player: "Player2", move: "9" }, 
+        { player: "Player1", move: "3" },
+        { player: "Player2", move: "5" },  // winning combination Player2
+        { player: "Player1", move: "7" }, 
+        { player: "Player2", move: "2" },  // winning combination Player2
+        { player: "Player1", move: "4" },
       ],
     }));
 
